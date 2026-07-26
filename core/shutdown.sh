f54e2434aaa5da4376e3f09ec4aefebca45b8cbb
Muteb Albalawi <muteb.blawi@gmail.com>
2026-07-26 13:35:29 +0300

    feat(core): add shutdown handler

---
+++ core/shutdown.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# ============================================================
+# مشروع        : Muteb SOC
+# الملف        : shutdown.sh
+# الوصف        : إنهاء الـ Framework.
+# ============================================================
+
+shutdown_framework() {
+
+    log_info "Stopping Muteb SOC..."
+
+    log_success "Shutdown completed."
+
+}
